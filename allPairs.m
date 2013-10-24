4d99749e00c7af7c536f573381d4bf23dff8c031
Tom Wilshere <tom.wilshere@gmail.com>
2013-10-24 16:25:44 +0100

    This makes no sense, but... It takes teh cross product of the given array, then selects the pairs of the result that aren't equal. Mind. Blown.

--- allPairs.m
+++ allPairs.m
@@ -1,6 +1,5 @@
 function pairs = allPairs(nodes)
 	[n1,n2] = meshgrid(nodes,nodes);
 	pairs = [n1(:), n2(:)];
-
-	%TODO: filter out the equal pairs 1,1 ; 2,2 etc.
+	pairs = pairs(pairs(:,1)~=pairs(:,2),:);
 end
