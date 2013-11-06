4aa0615f838f4cf9c6f46c25fe0391e668777ccd
Jack Stevenson <cogwirrel@gmail.com>
2013-11-06 18:05:07 +0000

    p1bc.m does both parts b and c - it creates 6 plots each with the raster of firing and the mean firing rates.

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 Computational Neurodynamics Coursework: Dynamical Complexity
 ============================================================
 
-1. a)	Plot in 'plots/p1a.fig'
-		Entry point for matlab code: 'p1a.m'
+1. a)		Plot is 'plots/connectivity.fig'
+			Entry point for matlab code: 'p1a.m'
 
-   b)	Plot in 'plots/p1b.fig'
-   		Entry point for matlab code: 'p1b.m'
+   b & c)	Plots are 	'plots/firing0-0.fig'
+   						'plots/firing0-1.fig'
+   						'plots/firing0-2.fig'
+   						'plots/firing0-3.fig'
+   						'plots/firing0-4.fig'
+   						'plots/firing0-5.fig'
+
+   			Entry point for matlab code: 'p1bc.m'
---
+++ p1bc.m
@@ -0,0 +1,85 @@
+% Question 1, part b and c.
+
+numExcitatory = 800;
+numInhibitory = 200;
+numCommunities = 8;
+numExcitatoryEdgesPerCommunity = 1000;
+ps = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5];
+
+Tmax = 1000;
+
+% Neuron types
+EXCITATORY = 1;
+INHIBITORY = 2;
+
+totalNodes = numExcitatory + numInhibitory;
+
+clf
+
+for p = 1:size(ps,2)
+
+	% Create a modular network following the algorithm given in Topic 9.
+	[network, nodes] = modular(numExcitatory, numInhibitory, numCommunities, numExcitatoryEdgesPerCommunity, ps(p));
+
+	% Set up our layers of neurons
+	layer = buildNeuronLayers(nodes, network, numExcitatory, numInhibitory);;
+
+	% Initialise layers firings
+	layer{EXCITATORY}.firings = [];
+	layer{INHIBITORY}.firings = [];
+
+	layer{EXCITATORY}.I = zeros(numExcitatory, 1);
+	layer{INHIBITORY}.I = zeros(numInhibitory, 1);
+
+	lambda = 0.01;
+
+	for t = 1:Tmax
+
+		% Display time every 1ms
+		t
+
+		% layer{EXCITATORY}.I = rand(numExcitatory, 1) * 5;
+		layer{INHIBITORY}.I = zeros(numInhibitory, 1);
+
+		poisson = poissrnd(lambda, numExcitatory, 1);
+		poisson(poisson > 0) = 1;
+		layer{EXCITATORY}.I = 15 * poisson;
+		% layer{INHIBITORY}.I = 20 * poissrnd(lambda, numInhibitory, 1);
+		
+		layer = IzNeuronUpdate(layer, EXCITATORY, t, 20);
+		layer = IzNeuronUpdate(layer, INHIBITORY, t, 20);
+	end
+
+	firings = layer{EXCITATORY}.firings;
+	windowSize = 50;
+	shiftAmount = 20;
+	meanFiringRates = zeros(numCommunities, ceil(Tmax/shiftAmount));
+	
+	for community = 1:numCommunities
+		communityFirings = firings([nodes(firings(:,2)).community] == community,:);
+		
+		for i = 1:shiftAmount:Tmax
+			numFirings = sum(communityFirings(:,1) >= i-windowSize & communityFirings(:,1) < i);
+			meanFiringRates(community, ceil(i/shiftAmount)) = numFirings / (windowSize*numExcitatory) * Tmax;
+		end
+
+	end
+
+	fig = figure(p);
+
+	% Raster plot of firings for 1.b)
+	subplot(2,1,1);
+	plot(layer{EXCITATORY}.firings(:,1),layer{EXCITATORY}.firings(:,2), '.');
+	title(sprintf('p = %0.1f', ps(p)));
+	xlabel('Time (ms) + 0s');
+	ylabel('Neuron Number');
+
+	% Plot of mean firing rates for 1.c)
+	subplot(2,1,2);
+	plot(1:shiftAmount:Tmax, meanFiringRates);
+	xlabel('Time (ms) + 0s');
+	ylabel('Mean Firing Rate');
+
+	saveas(fig, sprintf('plots/firing0-%d', p-1), 'fig');
+
+end
