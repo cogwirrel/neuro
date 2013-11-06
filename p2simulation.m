0d490d2612a62e3e29fa6e5c470d301a20b3cbb4
Jack Stevenson <cogwirrel@gmail.com>
2013-11-06 19:21:32 +0000

    p2simulation.m will run the Izecheviekechchkzkvch simulation for 20 trials of 60seconds for part 2. Saving the downsampled mean firing rates in p2data.mat. This is so we only have to run it once and I can mess around with the neural complexity part as much as I like :)

---
+++ p2simulation.m
@@ -0,0 +1,46 @@
+% Question 2
+% This runs the simulation to gather the data required for Question 2
+
+numTrials = 20;
+pMin = 0.1;
+pMax = 0.5;
+Tmax = 60000;
+
+% Network Configuration
+numEx = 800;
+numIn = 200;
+numCom = 8;
+numExEdgesPerCom = 1000;
+
+% Neuron types
+EXCITATORY = 1;
+INHIBITORY = 2;
+
+% Repeat process for the number of trials
+for trial = 1:numTrials
+
+	% Create a modular network with rewiring probability p between pMin and pMax
+	p = pMin + rand * (pMax - pMin);
+	disp(sprintf('Creating Modular Network for p = %0.4f', p));
+	[network, nodes] = modular(numEx, numIn, numCom, numExEdgesPerCom, p);
+
+	% Set up our layers of neurons
+	layer = buildNeuronLayers(nodes, network, numEx, numIn);;
+
+	% Run our simulation of the neural network
+	disp(sprintf('Running Izhikevich Simulation for %dms', Tmax));
+	layer = runIzSimulation(layer, Tmax);
+
+	% Downsample the mean firing rates of excitatory neurons
+	windowSize = 50;
+	shiftAmount = 20;
+	downsampledFiringRates = meanFiringRates(nodes, layer{EXCITATORY}.firings, numCom, numEx, Tmax, windowSize, shiftAmount);
+
+	% Trim the first second of firings from the results
+	firingRates(trial,:,:) = downsampledFiringRates(:,(1000/shiftAmount + 1):end);
+
+end
+
+% Save the data to a file
+% firingRates is indexed by (trial, community, firing rate)
+save('p2data.mat', 'firingRates');
