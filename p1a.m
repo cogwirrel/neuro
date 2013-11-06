3aa18e7a7cd5afbb8d18e4030b49db913e934f81
Jack Stevenson <cogwirrel@gmail.com>
2013-11-06 18:13:11 +0000

    Got rid of code not needed in p1a.m

--- p1a.m
+++ p1a.m
@@ -6,9 +6,6 @@ numCommunities = 8;
 numExcitatoryEdgesPerCommunity = 1000;
 ps = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5];
 
-totalNodes = numExcitatory + numInhibitory;
-networks = zeros(totalNodes, totalNodes, size(ps,2));
-
 clf
 fig1 = figure;
 
@@ -16,8 +13,8 @@ for i = 1:size(ps,2)
 	subplot(2,3,i);
 	% Create a modular network following the algorithm given in Topic 9.
 	[network, nodes] = modular(numExcitatory, numInhibitory, numCommunities, numExcitatoryEdgesPerCommunity, ps(i));
-	networks(:,:,i) = network;
-	spy(networks(:,:,i));
+
+	spy(network);
 	title(sprintf('p = %0.1f', ps(i)));
 	xlabel('Neuron Number (to)');
 	ylabel('Neuron Number (from)');
