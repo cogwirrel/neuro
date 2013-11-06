ea82baa39d4011b3cc24a570de0e7ed6c2025634
Jack Stevenson <cogwirrel@gmail.com>
2013-11-06 17:19:55 +0000

    p1b runs for all probabilities and generates the raster plots. It's neatly saved as plots/p1b.fig

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Computational Neurodynamics Coursework: Dynamical Complexity
 
 1. a)	Plot in 'plots/p1a.fig'
 		Entry point for matlab code: 'p1a.m'
+
+   b)	Plot in 'plots/p1b.fig'
+   		Entry point for matlab code: 'p1b.m'
--- p1b.m
+++ p1b.m
@@ -8,27 +8,22 @@ ps = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5];
 
 Tmax = 1000;
 
-
 % Neuron types
 EXCITATORY = 1;
 INHIBITORY = 2;
 
 totalNodes = numExcitatory + numInhibitory;
-layers = {};
 
 clf
+fig1 = figure;
 
-%for i = 1:size(ps,2)
+for i = 1:size(ps,2)
 
-	%subplot(5,4,i)
 	% Create a modular network following the algorithm given in Topic 9.
-	[network, nodes] = modular(numExcitatory, numInhibitory, numCommunities, numExcitatoryEdgesPerCommunity, 0);
-	
-	% have no connections (for testing)
-	% network = zeros(totalNodes,totalNodes);
+	[network, nodes] = modular(numExcitatory, numInhibitory, numCommunities, numExcitatoryEdgesPerCommunity, ps(i));
 
-	fixedLayer = buildNeuronLayers(nodes, network, numExcitatory, numInhibitory);
-	layer = fixedLayer;
+	% Set up our layers of neurons
+	layer = buildNeuronLayers(nodes, network, numExcitatory, numInhibitory);;
 
 	% Initialise layers firings
 	layer{EXCITATORY}.firings = [];
@@ -37,8 +32,6 @@ clf
 	layer{EXCITATORY}.I = zeros(numExcitatory, 1);
 	layer{INHIBITORY}.I = zeros(numInhibitory, 1);
 
-	v1 = [];
-	v2 = [];
 	lambda = 0.01;
 
 	for t = 1:Tmax
@@ -56,85 +49,14 @@ clf
 		
 		layer = IzNeuronUpdate(layer, EXCITATORY, t, 20);
 		layer = IzNeuronUpdate(layer, INHIBITORY, t, 20);
-	
-		v1(t,1:numExcitatory) = layer{1}.v;
-		v2(t,1:numInhibitory) = layer{2}.v;
-		
-		u1(t,1:numExcitatory) = layer{1}.u;
-		u2(t,1:numInhibitory) = layer{2}.u;
-
 	end
 
-	firings = layer{EXCITATORY}.firings;
-	windowSize = 50;
-	shiftAmount = 20;
-	meanFiringRates = zeros(ceil(Tmax/shiftAmount), numCommunities);
-	
-	for community = 1:numCommunities
-		communityFirings = firings([nodes(firings(:,2)).community] == community,:)
-		
-		for i = 1:shiftAmount:Tmax
-			numFirings = sum(communityFirings(:,1) >= i-windowSize & communityFirings(:,1) < i);
-			meanFiringRates(ceil(i/shiftAmount), community) = numFirings / (windowSize*numExcitatory) * Tmax;
-		end
-
-	end
-	
-	figure(4)
-	plot(1:shiftAmount:Tmax,meanFiringRates)
-
-figure(1)
-clf
-	subplot(2,1,1)
-plot(1:Tmax,v1)
-title('Population 1 membrane potentials')
-ylabel('Voltage (mV)')
-ylim([-90 40])
-
-	% figure(4)
-	% plot(1:shiftAmount:Tmax,meanFiringRates)
-
-% figure(1)
-% clf
-% 	subplot(2,1,1)
-% plot(1:Tmax,v1)
-% title('Population 1 membrane potentials')
-% ylabel('Voltage (mV)')
-% ylim([-90 40])
-% % xlabel('Time (ms)')
-
-% subplot(2,1,2)
-% plot(1:Tmax,v2)
-% % plot(1:Tmax,v2(:,1))
-% title('Population 2 membrane potentials')
-% ylabel('Voltage (mV)')
-% ylim([-90 40])
-% xlabel('Time (ms)')
-
-
-% Plot recovery variable
-
-% figure(2)
-% clf
-
-% subplot(2,1,1)
-% plot(1:Tmax,u1)
-% title('Population 1 recovery variables')
-% % xlabel('Time (ms)')
-
-% subplot(2,1,2)
-% plot(1:Tmax,u2)
-% % plot(1:Tmax,u2(:,1))
-% title('Population 2 recovery variables')
-% xlabel('Time (ms)')
-
-	figure(3)
-	clf
-	subplot(2,1,1);
+	subplot(3,2,i);
 	plot(layer{EXCITATORY}.firings(:,1),layer{EXCITATORY}.firings(:,2), '.');
-	subplot(2,1,2);
-	plot(layer{INHIBITORY}.firings(:,1),layer{INHIBITORY}.firings(:,2), '.');
-	%layers(i) = layer;
+	title(sprintf('p = %0.1f', ps(i)));
+	xlabel('Time (ms) + 0s');
+	ylabel('Neuron Number');
 
-%end
+end
 
+saveas(fig1, 'plots/p1b', 'fig');
